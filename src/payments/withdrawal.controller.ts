600bff1490beb3f030f716f84e03c3c7976c4bf8
khanbhai009-cloud <khanbhai9236716296@gmail.com>
2025-08-16 21:58:35 +0530

    Update withdrawal.controller.ts

--- src/payments/withdrawal.controller.ts
+++ src/payments/withdrawal.controller.ts
@@ -2,7 +2,8 @@ import { WithdrawalService } from './withdrawal.service';
 export class WithdrawalController {
   constructor(private withdrawalService: WithdrawalService) {}
 
-  async createWithdrawal(userId: string, coins: number) {
-    return this.withdrawalService.requestWithdrawal(userId, coins);
+  // Example method
+  requestWithdrawal(userId: string, amount: number) {
+    return this.withdrawalService.processWithdrawal(userId, amount);
   }
 }
